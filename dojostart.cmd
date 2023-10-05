c5096c921611cc3ca7d6ebbab1269f48ff358fc5
MichelMichels <michel@miche.ls>
2023-10-05 10:48:21 +0200

    Add services.msc, description to ServiceInstall

--- dojostart.cmd
+++ dojostart.cmd
@@ -1,6 +1,8 @@
 CALL C:\Install\dotnet\vars.cmd
 CALL C:\Install\dotnet\dotnet-desktop.cmd
 
-start "" "C:\dojo\BeltTest\BeltTestPackage\bin\x64\Debug\en-US"
 start appwiz.cpl
+start services.msc
+
+start "" "C:\dojo\BeltTest\BeltTestPackage\bin\x64\Debug\en-US"
 start "" "C:\Program Files"
